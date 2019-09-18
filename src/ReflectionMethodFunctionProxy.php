2771ad72c65a5e6522ea817b9df8fbd57fbfcf77
Johannes Hertenstein <johannes@nimius.net>
2019-09-18 14:39:12 +0200

    BUGFIX | Do not call parent constructor in proxy

--- src/ReflectionMethodFunctionProxy.php
+++ src/ReflectionMethodFunctionProxy.php
@@ -26,7 +26,6 @@ final class ReflectionMethodFunctionProxy extends \ReflectionMethod
      */
     public function __construct(string $className, string $methodName, \ReflectionFunction $reflectionFunction)
     {
-        parent::__construct($className, $methodName);
         $this->className = $className;
         $this->methodName = $methodName;
         $this->reflectionFunction = $reflectionFunction;
